3bae312601fa053911ba32ed962c8134739ad81c
Mikhail Dudarev <dev@mikhaildudarev.com>
2023-05-01 01:36:33 +0300

    add target for integration testing along with few tests

--- FunSwift/Package.swift
+++ FunSwift/Package.swift
@@ -19,6 +19,14 @@ let package = Package(
         .target(name: "Functions"),
         .target(name: "TestHelpers", path: "Tests/Helpers"),
         .testTarget(name: "OperatorsTests", dependencies: ["Operators", "TestHelpers"]),
-        .testTarget(name: "FunctionsTests", dependencies: ["Functions", "TestHelpers"])
+        .testTarget(name: "FunctionsTests", dependencies: ["Functions", "TestHelpers"]),
+        .testTarget(
+            name: "IntegrationTests",
+            dependencies: [
+                "Operators",
+                "Functions",
+                "TestHelpers"
+            ]
+        )
     ]
 )
---
+++ FunSwift/Tests/IntegrationTests/IntegrationTests.swift
@@ -0,0 +1,25 @@
+//
+//  IntegrationTests.swift
+//  IntegrationTests
+//
+//  Created by Mikhail Dudarev on 01.05.2023.
+//
+
+import Functions
+import Operators
+@testable import TestHelpers
+import XCTest
+
+final class IntegrationTests: XCTestCase {
+    func test() throws {
+        XCTAssertEqual(2 |> increment >>> square >>> String.init, "9")
+        XCTAssertEqual([1, 2, 3, 4] |> filter(isEven) >>> map(increment >>> square), [9, 25])
+        
+        XCTAssertEqual("Hello" |> flip(String.uppercased)(), "HELLO")
+        
+        let stringConstructor = flip(curry(String.init(data:encoding:)))
+        let utf8StringConstructor = stringConstructor(.utf8)
+        let stringData = "Hello".data(using: .utf8)!
+        XCTAssertEqual(utf8StringConstructor(stringData), "Hello")
+    }
+}
